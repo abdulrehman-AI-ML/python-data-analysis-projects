8c157f8920eb346d0164463736e826f4d725f4b8
Abdulrehman <abdulrehman5428ghj@gmail.com>
2026-01-19 01:55:28 +0500

    adding files

---
+++ Python/rent-calculater.py
@@ -0,0 +1,22 @@
+
+# ----Inputs we need from the user
+# Total rent
+rent = int(input("Enter the hostal rent: "))
+# Total food ordered for snacking
+food = int(input("Enter the amount of food orderad : "))
+# Electricity units spend
+electricity_Spend  = int(input("Enter the total of electricity spend: "))
+# Charge per unit
+charge_per_unit = int(input("Enter the charge per unit: "))
+#No of persons
+persons = int(input("Enter the number of persons living in room: "))
+#Total Bill
+total_bill = electricity_Spend * charge_per_unit
+
+##OutPut
+
+output = (food + rent + total_bill)//persons
+
+#Total amount you've to pay is 
+
+print('Each person will pay = ',output)
