15ea925995cc7bdf8a405c5ef55ce2a16ae3a8c4
Abdulrehman <abdulrehman5428ghj@gmail.com>
2026-01-24 00:38:33 +0500

    python project

---
+++ Python/gussing-number-game.py
@@ -0,0 +1,29 @@
+import random 
+
+computer = random.randint(1,5)
+print('You have 5 try to guess the number from 1 to 100')
+trys = 5
+
+while True:
+    try:
+        user = int(input("Guess the number: "))
+    except ValueError:
+        print("Pls enter a valid number")
+    if computer == user:
+        print(f'You guess correct {computer} is right!')
+        break
+    elif trys == 1:
+        print('You lose')
+        break
+    elif computer != user:
+        print(f'{user} is not correct...')
+        if computer > user:
+            print('Enter greater number')
+        elif computer < user:
+            print('Enter shorter number')
+        trys -=1
+        print(f'you left {trys} guss')
+        
+    
+
+    
