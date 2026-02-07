d8817c033f0bf8a173ab3c22658307604b840b7b
Abdulrehman <abdulrehman5428ghj@gmail.com>
2026-02-08 00:38:17 +0500

    add

--- Python/TO_DO_list.py
+++ Python/TO_DO_list.py
@@ -19,7 +19,7 @@ def delete():
     with open("file.txt", "r") as f:
         lines = f.readlines()
 
-    # Rewrite file without the deleted task
+    # Rewrite file without the deleted taska
     with open("file.txt", "w") as f:
         found = False
         for line in lines:
