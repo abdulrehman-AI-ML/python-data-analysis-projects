ed24b3888244cb096a271b3a028a2d3ff7048409
Abdulrehman <abdulrehman5428ghj@gmail.com>
2026-02-14 01:15:36 +0500

    file

---
+++ Python/ATM Machine Simulation.py
@@ -0,0 +1,11 @@
+main = input("1.New account 2.Existed account(1,2):\n>").lower()
+
+def new():
+    setpin = input("Enter your pin: ")
+    
+
+
+
+
+if main == '1' or main == "new account":
+    pin = input("set you pin:")
