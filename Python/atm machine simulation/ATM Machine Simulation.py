0f0f036fff49f7829e538fed2299095a437c3f4e
Abdulrehman <abdulrehman5428ghj@gmail.com>
2026-03-07 00:31:20 +0500

    add

--- Python/atm machine simulation/ATM Machine Simulation.py
+++ Python/atm machine simulation/ATM Machine Simulation.py
@@ -26,4 +26,6 @@ def new():
 
 if main == '1' or main == "new account":
     pin = input("set you pin:")
-    #addasdadsad
+    with open(f'M:\repos\python-data-analysis-projects\Python\atm machine simulation\accounts\{pin}','r') as f:
+    
+#addasdadsad
