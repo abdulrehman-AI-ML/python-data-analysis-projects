b8722317a1ac74eb295eed16518751f0d0793706
Abdulrehman <abdulrehman5428ghj@gmail.com>
2026-03-25 23:12:31 +0500

    conditions

--- Python/atm machine simulation/ATM Machine Simulation.py
+++ Python/atm machine simulation/ATM Machine Simulation.py
@@ -3,7 +3,14 @@ import time
 main = input("1.New account 2.Existed account(1,2):\n>").lower()
 
 
-def new():
+if main == "1" or main == 'new account':
+    new()
+elif main == '2' or main == 'existed account':
+    existed()
+
+
+
+def existed():
     setpin = input("Enter your pin: ")
     file_path=  fr"M:\repos\python-data-analysis-projects\Python\atm machine simulation\accounts\{setpin}.txt"
     with open (file_path,"r") as f:
@@ -19,8 +26,10 @@ def new():
             print(f.write(n_add_balance))
     else :
         print("Something is wrong!")
+    
 
-def New():
+
+def new():
     pin = input("set you pin:")
     with open(f'M:\repos\python-data-analysis-projects\Python\atm machine simulation\accounts\{pin}','r') as f:
         f.read()
@@ -34,3 +43,5 @@ def New():
             n_add_balance = int(input("Enter you balance:"))
             f.write(n_add_balance)
             print(f"{n_add_balance} has been added to your account")
+    else:
+        print('something wronge!!')
