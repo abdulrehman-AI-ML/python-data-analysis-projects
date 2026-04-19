cb6fba07ffe2a37e07433b47de7432c6ede5a482
Abdulrehman <abdulrehman5428ghj@gmail.com>
2026-04-20 01:15:08 +0500

    new feature

--- Python/password_manger/main.py
+++ Python/password_manger/main.py
@@ -16,3 +16,34 @@ def genrate_password():
     password = "".join(random.choice(chars) for _ in range(8))
     return password
 
+while True:
+    print("------ Password Manegar App ---------")
+
+    print("1- Add Password")
+    print("2- view password")
+    print("3- Genrate password")
+    print("4- Exit")
+
+    user  = input("Enter the your option: ").lower()
+
+    if user in ("1","add password"):
+        site = input("Enter website: ")
+        pwd = input("Enter Password: ")
+        password[site] = pwd
+        with open("file.txt","a") as f:
+            f.write(site+":"+pwd+"\n")
+        print("Password saved successfuly stored")
+
+
+    elif user in ("2","view password","view passwords"):
+        if not password:
+            print("no data found")
+        else:
+            view = input("Enter the site: ")
+            with open ("file.txt","r") as f:
+                for line in f:
+                    if view in line :
+                        print (f"{view.item()}")
+
+    elif user in ("4","exit",'stop'):
+        print("program ended")
